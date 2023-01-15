a37fe8b8e49f77664085c1f3c47ee7d3a888a029
vaishnavi <indra.467@gmail.com>
2023-01-15 14:57:25 +0530

    mongo

--- frontend.sh
+++ frontend.sh
@@ -1,79 +1,38 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
+source common.sh
 
 echo -e "\e[35m Install nginx\e[0m"
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 echo -e "\e[35m Enable nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status-check
 
 echo -e "\e[35m Start nginx\e[0m"
 systemctl start nginx &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 echo -e "\e[35m Remove nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 echo -e "\e[35m Download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m Extract frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 echo -e "\e[35m Copy roboshop nginx config file\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 echo -e "\e[35m Restart nginx\e[0m"
 systemctl restart nginx &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-exit
-fi
+status_check
 
 
 
