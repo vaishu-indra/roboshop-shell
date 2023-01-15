c6541d30be051e86ee0b568b7bc8b2ed87d603c3
vaishnavi <indra.467@gmail.com>
2023-01-15 11:29:01 +0530

    mongod

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,17 @@
 script_location=$(pwd)
+LOG=/tmp/roboshop.log
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[31m copying mongodb repo\e[0m"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 
-yum install mongodb-org -y
+echo -e "\e[31m Install mongodb\e[0m"
+yum install mongodb-org -y &>>${LOG}
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e "\e[31m changing bindip\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 
-systemctl enable mongod
-systemctl restart mongod
+echo -e "\e[31m enable mongodb\e[0m"
+systemctl enable mongod &>>${LOG}
+
+echo -e "\e[31m start mongodb\e[0m"
+systemctl restart mongod &>>${LOG}
