daf3dfaa9b4d7bc17bd61b55a5a9b8707d2fd749
vaishnavi <indra.467@gmail.com>
2023-01-15 15:17:51 +0530

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -1,22 +1,22 @@
 source common.sh
 
-echo -e "\e[35m copying mongodb repo\e[0m"
+print_head "copying mongodb repo"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
 
-echo -e "\e[35m Install mongodb\e[0m"
+print_head "Install mongodb"
 yum install mongodb-org -y &>>${LOG}
 status_check
 
-echo -e "\e[35m change bindip\e[0m"
+print_head "change bindip"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 status_check
 
-echo -e "\e[35m enable mongodb\e[0m"
+print_head "enable mongodb"
 systemctl enable mongod &>>${LOG}
 status_check
 
-echo -e "\e[35m restart mongodb\e[0m"
+print_head "restart mongodb"
 systemctl restart mongod &>>${LOG}
 status_check
 
